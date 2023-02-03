2954e6de10fbb8a133b67ac05a13d0ca81158eba
jameswang <james.wang.dev712@gmail.com>
2023-02-02 23:02:43 -0300

    feat: `HomeContent` component created

---
+++ src/components/HomeContent/index.tsx
@@ -0,0 +1,9 @@
+export function HomeContent(): JSX.Element {
+  return (
+    <div>
+      <p className="w-full text-gray-600 dark:text-gray-400 font-sans font-normal text-sm md:text-base">
+        Welcome to my place on the internet. I build things for the web.
+      </p>
+    </div>
+  )
+}
