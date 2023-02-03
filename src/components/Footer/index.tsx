a50a2afce3046ced1a51094cc726e31586771fb8
jameswang <james.wang.dev712@gmail.com>
2023-02-02 21:03:45 -0300

    style: changes in styled

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,12 +3,12 @@ import Link from "next/link"
 export function Footer(): JSX.Element {
   return (
     <footer className="w-full h-12 flex items-center justify-center">
-      <p className="text-white font-normal text-sm justify-center text-center md:text-base">
-        Built with Next.js, Tailwind and &#10084;&#65039; by&nbsp;
+      <p className="text-gray-600 dark:text-gray-400 font-normal text-sm justify-center text-center md:text-base ">
+        Built with Next.js and &#10084;&#65039; by&nbsp;
         <Link
           href="https://github.com/pauloruan"
           target="_blank"
-          className="no-underline hover:underline decoration-white decoration-2"
+          className="no-underline hover:underline decoration-white decoration-2 transition duration-300 ease-in-out text-gray-600 hover:text-black dark:text-gray-400 dark:hover:text-white"
         >
           Paulo Ruan
         </Link>
