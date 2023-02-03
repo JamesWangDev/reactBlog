df3961b112d5498feb4cbed0b2ee7908753b7028
jameswang <james.wang.dev712@gmail.com>
2023-02-02 21:16:27 -0300

    feat: dark mode implement

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
+import { ThemeProvider } from "next-themes"
 import type { AppProps } from "next/app"
 import "../styles/globals.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider enableSystem={true} attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,15 @@
 import Head from "next/head"
-
 import { Footer } from "../components/Footer"
+import { Header } from "../components/Header"
 
 export default function Home(): JSX.Element {
   return (
-    <div className="bg-black flex min-h-screen flex-col items-center justify-between py-2 px-8 md:py-4 md:px-16">
+    <div className="bg-gray-100 dark:bg-black flex min-h-screen flex-col items-center justify-between py-2 px-8 md:py-4 md:px-16">
       <Head>
         <title>pauloruan.io</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header />
       <div>
         <h1 className="text-white font-sans font-semibold text-lg">
           Paulo Ruan
