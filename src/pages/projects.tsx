e722512a63cc30f14a2f13a6accded1f693ffab1
jameswang <james.wang.dev712@gmail.com>
2023-02-02 23:10:59 -0300

    feat: `Projects` page created

---
+++ src/pages/projects.tsx
@@ -0,0 +1,21 @@
+import Head from "next/head"
+import { Footer } from "../components/Footer"
+import { Header } from "../components/Header"
+import { ProjectsContent } from "../components/ProjectsContent"
+import { SectionContainer } from "../components/SectionContainer"
+
+export default function Projects(): JSX.Element {
+  return (
+    <div className="bg-gray-100 dark:bg-black flex min-h-screen flex-col items-center justify-between py-2 px-8 md:py-4 md:px-16">
+      <Head>
+        <title>pauloruan.io</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <SectionContainer title="Projects">
+        <ProjectsContent />
+      </SectionContainer>
+      <Footer />
+    </div>
+  )
+}
