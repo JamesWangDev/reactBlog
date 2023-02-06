69bc92bb4cbf91a132ff8a85e0b3efce3eaef96b
jameswang <james.wang.dev712@gmail.com>
2023-02-06 13:49:14 -0300

    refactor: footer style and content changes

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,19 +1,13 @@
-import Link from "next/link"
+import { SocialContent } from "@components/SocialContent"
 
 export function Footer(): JSX.Element {
+  const currentYear = new Date().getFullYear()
   return (
-    <footer className="w-full h-12 flex items-center justify-center">
-      <p className="text-gray-600 dark:text-gray-400 font-normal text-sm justify-center text-center md:text-base ">
-        Built with Next.js and &#10084;&#65039; by&nbsp;
-        <Link
-          href="https://github.com/pauloruan"
-          target="_blank"
-          className="no-underline hover:underline decoration-white decoration-2 transition duration-300 ease-in-out text-gray-600 hover:text-black dark:text-gray-400 dark:hover:text-white"
-        >
-          Paulo Ruan
-        </Link>
-        .
+    <footer className="w-full h-12 flex flex-row flex-nowrap items-center justify-between text-center px-8 md:px-32 font-sans">
+      <p className="text-gray-600 dark:text-gray-400 font-sans font-normal text-sm justify-center text-center md:text-base">
+        {currentYear} | by Paulo Ruan
       </p>
+      <SocialContent />
     </footer>
   )
 }
