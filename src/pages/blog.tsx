9b57ef416bb1b016a02a79e11fb908be74845891
jameswang <james.wang.dev712@gmail.com>
2023-02-06 18:53:24 -0300

    fix: `HeadDefault` component remove

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,13 +1,17 @@
+import Head from "next/head"
 import { BlogContent } from "@components/BlogContent"
 import { Footer } from "@components/Footer"
-import { HeadDefault } from "@components/HeadDefault"
 import { Header } from "@components/Header"
 import { SectionContainer } from "@components/SectionContainer"
 
 export default function Blog(): JSX.Element {
   return (
     <div className="bg-gray-100 dark:bg-black flex min-h-screen flex-col items-center justify-between py-2 px-8 md:py-4 md:px-16">
-      <HeadDefault />
+      <Head>
+        <html lang="pt-br" />
+        <title>pauloruan.io</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Header />
       <SectionContainer title="Blog">
         <BlogContent />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
+import Head from "next/head"
 import { Footer } from "@components/Footer"
-import { HeadDefault } from "@components/HeadDefault"
 import { Header } from "@components/Header"
 import { HomeContent } from "@components/HomeContent"
 import { SectionContainer } from "@components/SectionContainer"
@@ -7,7 +7,11 @@ import { SectionContainer } from "@components/SectionContainer"
 export default function Home(): JSX.Element {
   return (
     <div className="bg-gray-100 dark:bg-black font-sans font-bold flex min-h-screen flex-col items-center justify-between py-2 px-8 md:py-4 md:px-16">
-      <HeadDefault />
+      <Head>
+        <html lang="pt-br" />
+        <title>pauloruan.io</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Header />
       <SectionContainer
         title="Paulo Ruan"
