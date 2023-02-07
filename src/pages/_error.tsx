99c70a7d40237c52c9ab87128def8a428a29f776
jameswang <james.wang.dev712@gmail.com>
2023-02-07 09:06:19 -0300

    style: Styling error page

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -1,10 +1,10 @@
 export default function Error({ statusCode }: { statusCode: number }) {
   return (
-    <p>
+    <h1 className="h-screen w-screen flex items-center justify-center font-sans font-black">
       {statusCode
         ? `An error ${statusCode} occurred on server`
         : "An error occurred on client"}
-    </p>
+    </h1>
   )
 }
 
