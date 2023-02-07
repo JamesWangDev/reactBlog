224f0f237b0030d868fe9a402ca9006b25e7adc1
jameswang <james.wang.dev712@gmail.com>
2023-02-07 10:17:13 -0300

    refactor: pages renamed to Portuguese

--- src/pages/sobre.tsx
+++ src/pages/sobre.tsx
@@ -1,11 +1,11 @@
 import Head from "next/head"
 
+import { AboutContent } from "../components/AboutContent"
 import { Footer } from "../components/Footer"
 import { Header } from "../components/Header"
-import { ProjectsContent } from "../components/ProjectsContent"
 import { SectionContainer } from "../components/SectionContainer"
 
-export default function Projects(): JSX.Element {
+export default function Sobre(): JSX.Element {
   return (
     <div className="bg-gray-100 dark:bg-black flex min-h-screen flex-col items-center justify-between py-2 px-8 md:py-4 md:px-16">
       <Head>
@@ -13,8 +13,8 @@ export default function Projects(): JSX.Element {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <SectionContainer title="Projetos">
-        <ProjectsContent />
+      <SectionContainer title="Sobre Mim">
+        <AboutContent />
       </SectionContainer>
       <Footer />
     </div>
