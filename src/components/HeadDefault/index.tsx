29d02f439fb297528f94e4eecc6a578da341ef74
jameswang <james.wang.dev712@gmail.com>
2023-02-07 10:08:55 -0300

    fix: remove tag `html`

--- src/components/HeadDefault/index.tsx
+++ src/components/HeadDefault/index.tsx
@@ -3,7 +3,6 @@ import Head from "next/head"
 export function HeadDefault(): JSX.Element {
   return (
     <Head>
-      <html lang="pt-BR" />
       <title>pauloruan.io</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
