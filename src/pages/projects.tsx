77c02f570bd27e02c8a6e06ce9f4eeced6960ddc
jameswang <james.wang.dev712@gmail.com>
2023-02-07 09:43:28 -0300

    fix: remove tag `html`

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -9,7 +9,6 @@ export default function Projects(): JSX.Element {
   return (
     <div className="bg-gray-100 dark:bg-black flex min-h-screen flex-col items-center justify-between py-2 px-8 md:py-4 md:px-16">
       <Head>
-        <html lang="pt-br" />
         <title>pauloruan.io</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
