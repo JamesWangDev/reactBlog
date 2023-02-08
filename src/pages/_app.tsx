904f4f7f8c645ca56e10f8dd8d5cd20e432502d2
jameswang <james.wang.dev712@gmail.com>
2023-02-08 20:01:02 -0300

    perf: `GlobalContext` implemented

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import { ThemeProvider } from "next-themes"
 import type { AppProps } from "next/app"
+
+import { GlobalProvider } from "../contexts/GlobalContext"
 import "../styles/globals.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider enableSystem={true} attribute="class">
-      <Component {...pageProps} />
+      <GlobalProvider>
+        <Component {...pageProps} />
+      </GlobalProvider>
     </ThemeProvider>
   )
 }
