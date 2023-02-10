9fe8ed0c62df06bc72ab71ea4f4cc4d045aafff0
jameswang <james.wang.dev712@gmail.com>
2023-02-10 11:27:51 -0300

    refactor: `sanity.client` moved to a `lib` folder

--- src/services/sanityClient.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "next-sanity"
-
-export const client = createClient({
-  projectId: process.env.SANITY_PROJECT_ID,
-  dataset: process.env.SANITY_DATASET,
-  apiVersion: process.env.SANITY_API_VERSION,
-  useCdn: false
-})
