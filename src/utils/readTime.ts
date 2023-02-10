6d812cc53119e77e1581ec4d26a82cbeb3b04860
jameswang <james.wang.dev712@gmail.com>
2023-02-10 17:59:37 -0300

    perf: function that will calculate the reading time created

---
+++ src/utils/readTime.ts
@@ -0,0 +1,19 @@
+import { Body } from "../../@types/global"
+
+export function countWords(blocks: Body[]): number[][] {
+  const words = blocks.map((block) => {
+    return block.children.map((child) => {
+      return child.text.split(" ").length
+    })
+  })
+  return words
+}
+
+export function readingTime(blocks: Body[]): string {
+  const words = countWords(blocks)
+  const wordsPerMinute = 225
+  const totalWords = words.flat().reduce((acc, curr) => acc + curr)
+  const readingTime = Math.ceil(totalWords / wordsPerMinute)
+  if (readingTime <= 1) return "1 minuto."
+  return `${readingTime} minutos.`
+}
