8f963a83c5a69701717f07b0dccb96ac7d87e828
jameswang <james.wang.dev712@gmail.com>
2023-02-10 18:42:28 -0300

    feat: `Loading` component created

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,7 @@
+export function Loading(): JSX.Element {
+  return (
+    <h3 className="h-screen w-screen flex items-center justify-center font-sans font-black dark:font-white">
+      Loading...
+    </h3>
+  )
+}
