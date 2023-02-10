888293673fe85f1e4a6319854abf8abb93ce7ee6
jameswang <james.wang.dev712@gmail.com>
2023-02-10 18:35:42 -0300

    perf: `NProgress` installed and configured

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,29 @@
 import { ThemeProvider } from "next-themes"
 import type { AppProps } from "next/app"
+import * as React from "react"
+import NProgress from "nprogress"
+import "nprogress/nprogress.css"
 
 import { GlobalProvider } from "../contexts/GlobalContext"
 import "../styles/globals.css"
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, router }: AppProps) {
+  React.useEffect(() => {
+    const handleRouteStart = () => NProgress.start()
+    const handleRouteDone = () => NProgress.done()
+
+    router.events.on("routeChangeStart", handleRouteStart)
+    router.events.on("routeChangeComplete", handleRouteDone)
+    router.events.on("routeChangeError", handleRouteDone)
+
+    return () => {
+      // Make sure to remove the event handler on unmount!
+      router.events.off("routeChangeStart", handleRouteStart)
+      router.events.off("routeChangeComplete", handleRouteDone)
+      router.events.off("routeChangeError", handleRouteDone)
+    }
+  })
+
   return (
     <ThemeProvider enableSystem={true} attribute="class">
       <GlobalProvider>
