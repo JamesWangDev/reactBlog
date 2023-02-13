fd709bb14469b1a64c211420d7eabc236c8af862
jameswang <james.wang.dev712@gmail.com>
2023-02-13 10:44:52 -0300

    refactor: changing the font `Roboto` to `Inter` and adding the color `cod-gray`

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,24 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'cod-gray': {
+          '50': '#f7f7f7',
+          '100': '#e3e3e3',
+          '200': '#c8c8c8',
+          '300': '#a4a4a4',
+          '400': '#818181',
+          '500': '#666666',
+          '600': '#515151',
+          '700': '#434343',
+          '800': '#383838',
+          '900': '#121212',
+        },
+      },
+    },
     fontFamily: {
-      sans: ["Roboto", "sans-serif"],
+      sans: ["Inter", "sans-serif"],
       code: ["Source Code Pro", "monospace"]
     }
   },
