c0babac435d5b613ad74a766177dca23f0d07fd5
jameswang <james.wang.dev712@gmail.com>
2023-02-13 10:36:47 -0300

    refactor: component style changes

--- src/components/HomeContent/index.tsx
+++ src/components/HomeContent/index.tsx
@@ -1,7 +1,7 @@
 export function HomeContent(): JSX.Element {
   return (
     <div>
-      <p className="w-full text-gray-600 dark:text-gray-400 font-sans font-normal text-sm md:text-base">
+      <p className="w-full text-gray-600 dark:text-gray-400 font-sans font-normal text-left py-1">
         Bem-vindo ao meu lugar na internet. Eu construo coisas para a web.
       </p>
     </div>
