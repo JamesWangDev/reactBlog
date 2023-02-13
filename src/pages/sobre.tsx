904c7b37b6c9de4472944d4abfc9a3ffff51edac
jameswang <james.wang.dev712@gmail.com>
2023-02-13 12:47:04 -0300

    refactor: `NotFound` page styled

--- src/pages/sobre.tsx
+++ src/pages/sobre.tsx
@@ -1,17 +1,13 @@
-import Head from "next/head"
-
 import { AboutContent } from "../components/AboutContent"
 import { Footer } from "../components/Footer"
+import { HeadDefault } from "../components/HeadDefault"
 import { Header } from "../components/Header"
 import { SectionContainer } from "../components/SectionContainer"
 
 export default function Sobre(): JSX.Element {
   return (
-    <div className="bg-gray-100 dark:bg-black flex min-h-screen flex-col items-center justify-between py-2 px-8 md:py-4 md:px-16">
-      <Head>
-        <title>pauloruan.io</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className="bg-cod-gray-100 dark:bg-cod-gray-900 flex min-h-screen w-full flex-col items-center justify-between py-2 px-4 md:py-4 md:px-16">
+      <HeadDefault />
       <Header />
       <SectionContainer title="Sobre Mim">
         <AboutContent />
