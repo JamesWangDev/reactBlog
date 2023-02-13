923a958a9f934fb7578b9c986307aedebc8f19ad
jameswang <james.wang.dev712@gmail.com>
2023-02-13 10:07:25 -0300

    refactor: component style changes

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import { SocialContent } from "../SocialContent"
 export function Footer(): JSX.Element {
   const currentYear = new Date().getFullYear()
   return (
-    <footer className="w-full h-12 flex flex-row flex-nowrap items-center justify-between text-center px-8 md:px-32 font-sans">
+    <footer className="box-border w-full h-12 flex flex-row flex-nowrap items-center justify-between text-center px-2 md:px-32 font-sans">
       <p className="text-gray-600 dark:text-gray-400 font-sans font-normal text-sm justify-center text-center md:text-base">
         {currentYear} | by Paulo Ruan
       </p>
