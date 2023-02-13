cea0cca4a29701850022c903051aacfe598023f8
jameswang <james.wang.dev712@gmail.com>
2023-02-13 12:31:32 -0300

    refactor: component style changes

--- src/components/SectionContainer/index.tsx
+++ src/components/SectionContainer/index.tsx
@@ -3,11 +3,11 @@ import { SectionContainerProps } from "../../../@types/global"
 export function SectionContainer(props: SectionContainerProps): JSX.Element {
   return (
     <section className="box-border w-full min-h-[50vh] flex flex-col items-start justify-center py-1 px-2 md:py-4 md:px-32">
-      <h1 className="w-full text-black dark:text-white font-sans text-5xl font-extrabold text-left">
+      <h1 className="w-full text-black dark:text-white font-sans text-5xl font-extrabold text-left py-1">
         {props.title}
       </h1>
       {props.subtitle && (
-        <h2 className="w-full text-gray-600 dark:text-gray-400 font-sans text-4xl font-bold text-left">
+        <h2 className="w-full text-gray-600 dark:text-gray-400 font-sans text-4xl font-bold text-left py-1">
           {props.subtitle}
         </h2>
       )}
