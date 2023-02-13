87fcf2a3a44d3fd072ef4ab9a201e18538197ed9
jameswang <james.wang.dev712@gmail.com>
2023-02-13 10:09:26 -0300

    refactor: component style changes

--- src/components/MenuItem/index.tsx
+++ src/components/MenuItem/index.tsx
@@ -6,7 +6,7 @@ export function MenuItem(props: NavLink): JSX.Element {
     <Link
       href={props.path}
       target="_self"
-      className="h-8 w-16 no-underline text-sm font-medium cursor-pointer p-1 mx-1 rounded-lg flex items-center justify-center transition duration-300 ease-in-out text-gray-600 hover:text-black dark:text-gray-400 dark:hover:text-white"
+      className="h-8 w-16 no-underline text-sm md:text-base font-medium cursor-pointer p-1 mx-1 rounded-sm flex items-center justify-center transition duration-300 ease-in-out text-gray-600 hover:text-black dark:text-gray-400 dark:hover:text-white"
     >
       {props.name}
     </Link>
