8d92eaaaac049dd9b36f9f4dcc90e67ef60f2fd4
jameswang <james.wang.dev712@gmail.com>
2023-02-14 18:07:35 -0300

    feat: `blog` page created

---
+++ src/pages/blog/[slug].tsx
@@ -0,0 +1,54 @@
+import { useRouter } from "next/router"
+import { FormattedPost, PostProps } from "../../../@types/global"
+import { Footer } from "../../components/Footer"
+import { HeadDefault } from "../../components/HeadDefault"
+import { Header } from "../../components/Header"
+import { Loading } from "../../components/Loading"
+import { PostContainer } from "../../components/PostContainer"
+import { client } from "../../lib/sanity.client"
+import { convertToBrazilianDate } from "../../utils/convertToBrazilianDate"
+import { sanityQueries } from "../../utils/sanityQueries"
+
+export async function getStaticProps(context: any) {
+  const { slug } = context.params
+  const [postBySlug] = await client.fetch(sanityQueries.postBySlug, { slug })
+  const post = {
+    ...postBySlug,
+    date: convertToBrazilianDate(postBySlug.date)
+  }
+  return {
+    props: {
+      post
+    }
+  }
+}
+
+export async function getStaticPaths() {
+  const posts = await client.fetch(sanityQueries.slugs)
+
+  const paths = posts.map((post: FormattedPost) => ({
+    params: { slug: post.slug }
+  }))
+
+  return {
+    paths,
+    fallback: true
+  }
+}
+
+export default function Post(props: PostProps): JSX.Element {
+  const router = useRouter()
+
+  if (router.isFallback) {
+    return <Loading />
+  }
+
+  return (
+    <div className="bg-cod-gray-100 dark:bg-cod-gray-900 flex min-h-screen w-full flex-col items-center justify-between py-2 px-4 md:py-4 md:px-16">
+      <HeadDefault />
+      <Header />
+      <PostContainer post={props.post} />
+      <Footer />
+    </div>
+  )
+}
---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,55 @@
+import * as React from "react"
+import {
+  BlogGetStaticPropsResponse,
+  BlogProps,
+  FormattedPost
+} from "../../../@types/global"
+import { BlogContent } from "../../components/BlogContent"
+import { Footer } from "../../components/Footer"
+import { HeadDefault } from "../../components/HeadDefault"
+import { Header } from "../../components/Header"
+import { SectionContainer } from "../../components/SectionContainer"
+import { GlobalContext } from "../../contexts/GlobalContext"
+import { client } from "../../lib/sanity.client"
+import { convertToBrazilianDate } from "../../utils/convertToBrazilianDate"
+import { sanityQueries } from "../../utils/sanityQueries"
+
+export async function getStaticProps(): Promise<BlogGetStaticPropsResponse> {
+  const posts = await client.fetch(sanityQueries.posts)
+  const formattedPosts = posts.map((post: FormattedPost) => {
+    return { ...post, date: convertToBrazilianDate(post.date) }
+  })
+
+  return {
+    props: {
+      formattedPosts
+    }
+  }
+}
+
+export default function Blog(props: BlogProps): JSX.Element {
+  const { setPosts, searchValue } = React.useContext(GlobalContext)
+
+  React.useEffect(() => {
+    setPosts(props.formattedPosts)
+  })
+
+  return (
+    <div className="bg-cod-gray-100 dark:bg-cod-gray-900 flex min-h-screen w-full flex-col items-center justify-between py-2 px-4 md:py-4 md:px-16">
+      <HeadDefault />
+      <Header />
+      <SectionContainer
+        title="Blog"
+        // subtitle="Todos os posts"
+        subtitle={
+          searchValue.length > 0
+            ? `Resultados para "${searchValue}"`
+            : "Todos os posts"
+        }
+      >
+        <BlogContent />
+      </SectionContainer>
+      <Footer />
+    </div>
+  )
+}
