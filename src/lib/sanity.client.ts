296f0a51542b0fb94efa57327687a22913448251
jameswang <james.wang.dev712@gmail.com>
2023-02-14 11:17:58 -0300

    refactor: remove export const

--- src/lib/sanity.client.ts
+++ src/lib/sanity.client.ts
@@ -1,8 +1,8 @@
 import { createClient } from "next-sanity"
 
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
-export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION
 
 export const client = createClient({
   projectId,
