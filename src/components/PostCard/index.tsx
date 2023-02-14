2a330b81a5b99dde673ea4ec7efdc4337983df36
jameswang <james.wang.dev712@gmail.com>
2023-02-14 09:35:05 -0300

    refactor: component style changes

--- src/components/PostCard/index.tsx
+++ src/components/PostCard/index.tsx
@@ -8,7 +8,7 @@ import { readingTime } from "../../utils/readTime"
 export function PostCard(props: FormattedPost): JSX.Element {
   const builder = imageUrlBuilder(client)
   return (
-    <div className="max-w-xs bg-cod-gray-100 hover:bg-white border border-cod-gray-200 rounded-sm shadow dark:bg-cod-gray-900 dark:hover:bg-cod-gray-800 dark:border-cod-gray-700 transition duration-300 ease-in-out">
+    <div className="bg-cod-gray-100 max-w-xs border border-cod-gray-200 rounded-sm overflow-hidden shadow-md dark:bg-cod-gray-900 dark:border-cod-gray-700 transition duration-300 ease-in-out hover:scale-105">
       <Link href={`/blog/${props.slug}`}>
         <Image
           src={builder.image(props.poster).url()}
@@ -21,10 +21,23 @@ export function PostCard(props: FormattedPost): JSX.Element {
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
             {props.title}
           </h5>
+          <p className="mb-2 text-gray-700 dark:text-gray-400">
+            {props.subtitle}
+          </p>
           <p className="font-normal text-gray-700 dark:text-gray-400">
             Tempo de leitura:&nbsp;
             {readingTime(props.body)}
           </p>
+          <div className="flex flex-row pt-4 pb-2">
+            {props.categories.map((category) => (
+              <span
+                key={category}
+                className="inline-block bg-gray-400 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 dark:bg-cod-gray-700 dark:text-gray-400"
+              >
+                #{category.toLowerCase().replace(" ", "-")}
+              </span>
+            ))}
+          </div>
         </div>
       </Link>
     </div>
