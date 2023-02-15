7b322c38e94b78f624511317c90664c851ec1afc
jameswang <james.wang.dev712@gmail.com>
2023-02-15 10:43:10 -0300

    refactor: query changes

--- src/utils/sanityQueries.ts
+++ src/utils/sanityQueries.ts
@@ -1,24 +1,24 @@
 export const sanityQueries = {
-  posts: `*[_type == 'post'] {
+  posts: `*[_type == 'post' && published == true] {
     title,
     subtitle,
     "slug": slug.current,
     "poster": mainImage.asset._ref,
     "attribution": mainImage.attribution,
-    "categories": *[_type == 'category'].category,
+    "categories": *[_type == 'category'].slug.current,
     "authorName": author -> name,
     "authorImage": author -> image.asset._ref,
     body,
     date,
     published
-  } | order(_createdAt desc)`,
+  }`,
   postBySlug: `*[_type == "post" && slug.current == $slug] {
     title,
     subtitle,
     "slug": slug.current,
     "poster": mainImage.asset._ref,
     "attribution": mainImage.attribution,
-    "categories": *[_type == 'category'].category,
+    "categories": *[_type == 'category'].slug.current,
     "authorName": author -> name,
     "authorImage": author -> image.asset._ref,
     body,
