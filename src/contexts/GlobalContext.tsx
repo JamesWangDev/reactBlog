71912ea82e3a579deda3b7de6b8a0dff06208604
jameswang <james.wang.dev712@gmail.com>
2023-02-15 11:04:21 -0300

    style: fix in `React` and type import

--- src/contexts/GlobalContext.tsx
+++ src/contexts/GlobalContext.tsx
@@ -1,10 +1,9 @@
-import React, { createContext, useMemo, useState } from "react"
-
-import { PostFormatted } from "../pages/blog"
+import * as React from "react"
+import { FormattedPost } from "../../@types/global"
 
 interface GlobalContextProps {
-  posts: PostFormatted[]
-  setPosts: React.Dispatch<React.SetStateAction<PostFormatted[]>>
+  posts: FormattedPost[]
+  setPosts: React.Dispatch<React.SetStateAction<FormattedPost[]>>
   searchValue: string
   setSearchValue: React.Dispatch<React.SetStateAction<string>>
 }
@@ -16,17 +15,18 @@ const DEFAULT_VALUE = {
   setSearchValue: () => {}
 }
 
-export const GlobalContext = createContext<GlobalContextProps>(DEFAULT_VALUE)
+export const GlobalContext =
+  React.createContext<GlobalContextProps>(DEFAULT_VALUE)
 
 export function GlobalProvider({
   children
 }: {
   children: React.ReactNode
 }): JSX.Element {
-  const [posts, setPosts] = useState<PostFormatted[]>([])
-  const [searchValue, setSearchValue] = useState<string>("")
+  const [posts, setPosts] = React.useState<FormattedPost[]>([])
+  const [searchValue, setSearchValue] = React.useState<string>("")
 
-  const memoizedValue = useMemo(
+  const memoizedValue = React.useMemo(
     () => ({
       posts,
       setPosts,
