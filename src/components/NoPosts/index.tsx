52bb5faa08ab9d6f8764058300b1c5967d92abf5
jameswang <james.wang.dev712@gmail.com>
2023-02-16 19:55:00 -0300

    refactor: component style changes

--- src/components/NoPosts/index.tsx
+++ src/components/NoPosts/index.tsx
@@ -1,6 +1,6 @@
 export function NoPosts(): JSX.Element {
   return (
-    <div className="w-full min-h-[418px] flex justify-center items-center">
+    <div className="w-full min-h-[221px] flex justify-center items-center my-2 py-2">
       <h3 className="text-gray-600 dark:text-gray-400 font-sans my-1 font-bold">
         Nenhum post encontrado.
       </h3>
