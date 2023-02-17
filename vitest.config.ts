d8e4f3f7df1da465546b82a3a4fa08d9855c09d7
jameswang <james.wang.dev712@gmail.com>
2023-02-17 15:25:04 -0300

    perf: vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,8 @@
 /// <reference types="vitest" />
 
-import { alias } from "./src/utils/alias"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vitest/config"
+import { alias } from "./src/utils/alias"
 
 // https://vitejs.dev/config/
 export default defineConfig({
