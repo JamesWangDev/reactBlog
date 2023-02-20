dc0b9449972094eca473602f214627b10f48fe2c
jameswang <james.wang.dev712@gmail.com>
2023-02-20 15:08:20 -0300

    refactor: component content changes

--- src/components/SocialContent/index.tsx
+++ src/components/SocialContent/index.tsx
@@ -3,23 +3,27 @@ import { FaGithub, FaLinkedin } from "react-icons/fa"
 
 export function SocialContent(): JSX.Element {
   return (
-    <div className="flex flex-row items-center justify-between w-auto h-full">
-      <Link
-        href="https://linkedin.com/in/pauloruan/"
-        target="_blank"
-        aria-label="LinkedIn Profile"
-        className="text-gray-600 dark:text-gray-400 hover:text-black dark:hover:text-white transition duration-300 ease-in-out"
-      >
-        <FaLinkedin className="text-lg mr-1 md:text-2xl md:mr-2" />
-      </Link>
-      <Link
-        href="https://github.com/pauloruan/"
-        target="_blank"
-        aria-label="GitHub Profile"
-        className="text-gray-600 dark:text-gray-400 hover:text-black dark:hover:text-white transition duration-300 ease-in-out"
-      >
-        <FaGithub className="text-lg ml-1 md:text-2xl md:ml-2" />
-      </Link>
-    </div>
+    <ul className="flex flex-row items-center justify-between w-auto h-full">
+      <li className="mr-1 md:mr-2 list-none">
+        <Link
+          href="https://linkedin.com/in/pauloruan/"
+          target="_blank"
+          aria-label="LinkedIn Profile"
+          className="text-gray-600 dark:text-gray-400 hover:text-black dark:hover:text-white transition duration-300 ease-in-out"
+        >
+          <FaLinkedin className="text-lg mr-1 md:text-2xl md:mr-2" />
+        </Link>
+      </li>
+      <li className="ml-1 md:ml-2 list-none">
+        <Link
+          href="https://github.com/pauloruan/"
+          target="_blank"
+          aria-label="GitHub Profile"
+          className="text-gray-600 dark:text-gray-400 hover:text-black dark:hover:text-white transition duration-300 ease-in-out"
+        >
+          <FaGithub className="text-lg ml-1 md:text-2xl md:ml-2" />
+        </Link>
+      </li>
+    </ul>
   )
 }
