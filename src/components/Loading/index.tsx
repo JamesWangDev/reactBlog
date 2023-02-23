0782c6dac490074ef8e2c1941d3e46dae7380a30
jameswang <james.wang.dev712@gmail.com>
2023-02-23 16:34:37 -0300

    perf: text translated into Portuguese

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,7 +1,7 @@
 export function Loading(): JSX.Element {
   return (
     <h3 className="h-screen w-screen flex items-center justify-center font-sans text-3xl font-bold animate-pulse dark:font-white">
-      Loading...
+      Carregando...
     </h3>
   )
 }
