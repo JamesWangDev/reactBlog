8ea582b8ed898d647bda81786a8d985baeccf21b
jameswang <james.wang.dev712@gmail.com>
2023-02-23 16:11:19 -0300

    perf: seo settings

--- src/components/HeadDefault/index.tsx
+++ src/components/HeadDefault/index.tsx
@@ -4,6 +4,15 @@ export function HeadDefault(): JSX.Element {
   return (
     <Head>
       <title>Paulo Ruan</title>
+      <meta property="og:title" content="Paulo Ruan" />
+      <meta
+        name="description"
+        content="Portfolio de Paulo Ruan - Desenvolvedor Full Stack"
+      />
+      <meta property="og:url" content="https://pauloruan.vercel.app/" />
+      <meta property="og:type" content="website" />
+      <meta name="author" content="Paulo Ruan" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
   )
