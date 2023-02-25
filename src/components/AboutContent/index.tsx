cbe0cde98997a246484ed58862c43a12fe3fcd3b
jameswang <james.wang.dev712@gmail.com>
2023-02-25 18:16:48 -0300

    perf: added `aria-label` tags

--- src/components/AboutContent/index.tsx
+++ src/components/AboutContent/index.tsx
@@ -6,6 +6,7 @@ export function AboutContent(): JSX.Element {
     <div className="w-full h-full flex flex-col items-start justify-start mt-2">
       <div className="h-full min-w-fit flex flex-col text-center justify-center items-start md:items-center mb-4 md:flex-row">
         <Image
+          aria-label="Profile Picture"
           src="https://avatars.githubusercontent.com/u/85769241?v=4"
           alt="Foto de Paulo Ruan"
           width={120}
@@ -13,20 +14,32 @@ export function AboutContent(): JSX.Element {
           className="rounded-full md:mr-4 my-4"
         />
         <div className="flex flex-col items-start justify-center text-center">
-          <h3 className="text-3xl font-bold text-black dark:text-white font-sans capitalize">
+          <h3
+            aria-label="Name"
+            className="text-3xl font-bold text-black dark:text-white font-sans capitalize"
+          >
             Paulo Ruan
           </h3>
-          <p className="text-gray-600 dark:text-gray-400 font-normal text-sm w-44 flex justify-start items-center text-center md:text-base capitalize py-1">
+          <p
+            aria-label="Job"
+            className="text-gray-600 dark:text-gray-400 font-normal text-sm w-44 flex justify-start items-center text-center md:text-base capitalize py-1"
+          >
             <MdWork className="mr-1" />
             Open Source
           </p>
-          <p className="text-gray-600 dark:text-gray-400 font-normal text-sm w-44 flex justify-start items-center text-center md:text-base capitalize py-1">
+          <p
+            aria-label="Location"
+            className="text-gray-600 dark:text-gray-400 font-normal text-sm w-44 flex justify-start items-center text-center md:text-base capitalize py-1"
+          >
             <MdLocationOn className="mr-1" />
             Baturité/CE - Brasil
           </p>
         </div>
       </div>
-      <p className="text-black dark:text-white font-normal flex justify-center text-start">
+      <p
+        aria-label="About"
+        className="text-black dark:text-white font-normal flex justify-center text-start"
+      >
         Bem-vindo ao meu site! Eu uso isso para mostrar alguns de meus projetos,
         testar algumas coisas e às vezes escrever sobre meu aprendizado ou sobre
         carreira no meu blog. Sou um desenvolvedor Full stack e atualmente moro
