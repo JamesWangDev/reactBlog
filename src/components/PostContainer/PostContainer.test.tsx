cd7ae0c5ec82ddcb713dbade1d65439ad0aae257
jameswang <james.wang.dev712@gmail.com>
2023-02-25 17:16:16 -0300

    perf: imported and using `mocks`

--- src/components/PostContainer/PostContainer.test.tsx
+++ src/components/PostContainer/PostContainer.test.tsx
@@ -1,33 +1,7 @@
 import { PostContainer } from "@components/PostContainer"
 import { render, screen, within } from "@testing-library/react"
+import { mockPost } from "@utils/mocks"
 import { describe, expect, test } from "vitest"
-
-const mockPost = {
-  authorImage: "image-1f9dbf48cec378caa3c23908cf797a86e8422e00-460x460-jpg",
-  authorName: "Paulo Ruan",
-  body: [
-    {
-      _key: "a23c04be1dc2",
-      _type: "block",
-      children: [
-        {
-          _key: "c82a8f18c7a10",
-          _type: "span",
-          marks: [],
-          text: "texto de teste"
-        }
-      ],
-      markDefs: [],
-      style: "normal"
-    }
-  ],
-  categories: ["categoria-teste"],
-  date: "2023-02-08",
-  published: true,
-  slug: "meu-primeiro-teste",
-  subtitle: "testando o post container",
-  title: "Meu Primeiro Teste"
-}
 
 describe("PostContainer", (): void => {
   test("renders the post container", (): void => {
