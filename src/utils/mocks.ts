d1f324baa4ed32e760879f4b8ede71761f280402
jameswang <james.wang.dev712@gmail.com>
2023-02-25 17:13:52 -0300

    feat: `mocks` created

---
+++ src/utils/mocks.ts
@@ -0,0 +1,36 @@
+export const mockPost = {
+  authorImage: "image-1f9dbf48cec378caa3c23908cf797a86e8422e00-460x460-jpg",
+  authorName: "Paulo Ruan",
+  body: [
+    {
+      _key: "a23c04be1dc2",
+      _type: "block",
+      children: [
+        {
+          _key: "c82a8f18c7a10",
+          _type: "span",
+          marks: [],
+          text: "texto de teste"
+        }
+      ],
+      markDefs: [],
+      style: "normal"
+    }
+  ],
+  categories: ["categoria-teste"],
+  date: "2023-02-08",
+  published: true,
+  slug: "meu-primeiro-teste",
+  subtitle: "testando o post container",
+  title: "Meu Primeiro Teste"
+}
+
+export const projectMock = {
+  name: "Portfólio",
+  description: "Portfólio Pessoal de Paulo Ruan",
+  slug: "portfolio",
+  repository: "https://github.com/pauloruan/pauloruan.io",
+  website: "https://pauloruan.vercel.app",
+  image: "image-1f9dbf48cec378caa3c23908cf797a86e8422e00-460x460-jpg",
+  technologies: ["TypeScript", "Next.js"]
+}
