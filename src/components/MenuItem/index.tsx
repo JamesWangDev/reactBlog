bbf0f30ac8f95897b2a5134b1db331435b0fc1d7
jameswang <james.wang.dev712@gmail.com>
2023-02-25 15:21:27 -0300

    perf: change in imports path and addition of `aria-label` property

--- src/components/MenuItem/index.tsx
+++ src/components/MenuItem/index.tsx
@@ -1,9 +1,10 @@
+import type { NavLink } from "@types"
 import Link from "next/link"
-import { NavLink } from "../../../@types/global"
 
 export function MenuItem(props: NavLink): JSX.Element {
   return (
     <Link
+      aria-label={props.name}
       href={props.path}
       target="_self"
       className="h-8 w-16 no-underline text-sm md:text-base font-medium cursor-pointer p-1 mx-1 rounded-sm flex items-center justify-center transition duration-300 ease-in-out text-gray-600 hover:text-black dark:text-gray-400 dark:hover:text-white"
