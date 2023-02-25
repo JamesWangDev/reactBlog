ebc7d9cbe09848437e1c0a859d6c51935e0bc3b9
jameswang <james.wang.dev712@gmail.com>
2023-02-25 17:18:50 -0300

    perf: removing subtitle

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -5,16 +5,18 @@ import { Header } from "@components/Header"
 import { SectionContainer } from "@components/SectionContainer"
 import { GlobalContext } from "@contexts/GlobalContext"
 import { client } from "@lib/sanity.client"
-import { BlogGetStaticPropsResponse, BlogProps, FormattedPost } from "@types"
+import { BlogGetStaticProps, BlogProps, FormattedPost } from "@types"
 import { convertToBrazilianDate } from "@utils/convertToBrazilianDate"
 import { sanityQueries } from "@utils/sanityQueries"
 import * as React from "react"
 
-export async function getStaticProps(): Promise<BlogGetStaticPropsResponse> {
+export async function getStaticProps(): Promise<BlogGetStaticProps> {
   const posts = await client.fetch(sanityQueries.posts)
-  const formattedPosts = posts.map((post: FormattedPost) => {
-    return { ...post, date: convertToBrazilianDate(post.date) }
-  })
+  const formattedPosts = posts
+    .filter((post: FormattedPost) => post.published)
+    .map((post: FormattedPost) => {
+      return { ...post, date: convertToBrazilianDate(post.date) }
+    })
 
   return {
     props: {
@@ -24,7 +26,7 @@ export async function getStaticProps(): Promise<BlogGetStaticPropsResponse> {
 }
 
 export default function Blog(props: BlogProps): JSX.Element {
-  const { setPosts, searchValue } = React.useContext(GlobalContext)
+  const { setPosts } = React.useContext(GlobalContext)
 
   React.useEffect(() => {
     setPosts(props.formattedPosts)
@@ -34,14 +36,7 @@ export default function Blog(props: BlogProps): JSX.Element {
     <div className="bg-cod-gray-100 dark:bg-cod-gray-900 flex min-h-screen w-full flex-col items-center justify-between py-2 px-4 md:py-4 md:px-16">
       <HeadDefault />
       <Header />
-      <SectionContainer
-        title="Blog"
-        subtitle={
-          searchValue.length > 0
-            ? `Resultados para "${searchValue}"`
-            : "Todos os posts"
-        }
-      >
+      <SectionContainer title="Blog">
         <BlogContent />
       </SectionContainer>
       <Footer />
