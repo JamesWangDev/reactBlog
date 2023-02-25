cdc0a319ea2fdd5b26e952d32fe25bb952bb371e
jameswang <james.wang.dev712@gmail.com>
2023-02-25 15:54:38 -0300

    perf: change the display of components

--- src/components/BlogContent/index.tsx
+++ src/components/BlogContent/index.tsx
@@ -1,24 +1,15 @@
 import { NoPosts } from "@components/NoPosts"
 import { PostsList } from "@components/PostsList"
-import { SearchPost } from "@components/SearchPost"
 import { GlobalContext } from "@contexts/GlobalContext"
-import { FormattedPost } from "@types"
 import * as React from "react"
 
 export function BlogContent(): JSX.Element {
-  const { posts, searchValue } = React.useContext(GlobalContext)
-  const allPosts = posts
-  const filteredPosts = searchValue
-    ? allPosts.filter((post: FormattedPost) => {
-        return post.title.toLowerCase().includes(searchValue.toLowerCase())
-      })
-    : posts
+  const { posts } = React.useContext(GlobalContext)
 
   return (
     <div className="w-full flex flex-col justify-between items-start min-h-[280px]">
-      <SearchPost />
-      {filteredPosts.length === 0 && <NoPosts />}
-      {filteredPosts.length > 0 && <PostsList posts={filteredPosts} />}
+      {posts.length === 0 && <NoPosts />}
+      {posts.length > 0 && <PostsList posts={posts} />}
     </div>
   )
 }
