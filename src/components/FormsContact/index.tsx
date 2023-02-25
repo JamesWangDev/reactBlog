55677c7d31c281d484a0bfd1c1189d3f901e76ac
jameswang <james.wang.dev712@gmail.com>
2023-02-25 13:17:48 -0300

    refactor: changes to component content and style

--- src/components/FormsContact/index.tsx
+++ src/components/FormsContact/index.tsx
@@ -1,21 +1,13 @@
+import type { FormContactProps } from "@types"
+import { sendEmail } from "@utils/sendEmail"
 import * as React from "react"
 import ReCAPTCHA from "react-google-recaptcha"
 import { useForm } from "react-hook-form"
 import { RiSendPlaneFill } from "react-icons/ri"
-import emailjs from "@emailjs/browser"
-
-type FormContactProps = {
-  name: string
-  email: string
-  message: string
-}
 
 export function FormContact(): JSX.Element {
-  const serviceId = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID as string
-  const templateId = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID as string
-  const publicKey = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY as string
   const siteKey = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY as string
-  const [recaptchaFailed, setRecaptchaFailed] = React.useState(false)
+  const [recaptchaSubmitted, setRecaptchaSubmitted] = React.useState(false)
   const [formSubmitted, setFormSubmitted] = React.useState(false)
   const [sendingForm, setSendingForm] = React.useState(false)
   const recaptchaRef = React.useRef<ReCAPTCHA>(null)
@@ -26,33 +18,44 @@ export function FormContact(): JSX.Element {
     formState: { errors, isDirty, isValid }
   } = useForm<FormContactProps>()
 
-  function sendEmail(data: FormContactProps) {
-    const templateParams = {
-      from_name: data.name,
-      email: data.email,
-      message: data.message
-    }
-    try {
-      emailjs.send(serviceId, templateId, templateParams, publicKey)
-      console.log("SUCCESS!")
-      return true
-    } catch (error) {
-      console.log("FAILED...", error)
-      return false
-    }
+  function resetFormFields() {
+    resetField("email")
+    resetField("name")
+    resetField("message")
   }
 
-  function handleForm(data: FormContactProps) {
-    setSendingForm(true)
-    const sendResult = sendEmail(data)
-    if (sendResult) {
-      resetField("email")
-      resetField("name")
-      resetField("message")
+  async function recaptchaValidation() {
+    const token = recaptchaRef.current?.getValue()
+    const response = await fetch("/api/verify-token", {
+      method: "POST",
+      body: JSON.stringify({ value: token })
+    })
+    const validation = await response.json()
+    if (validation.success) {
+      console.log(validation.success)
+      return token
+    } else throw new Error("ReCAPTCHA precisa ser resolvido")
+  }
+
+  async function handleForm(data: FormContactProps) {
+    try {
+      setSendingForm(true)
+      await recaptchaValidation()
+      await sendEmail(data)
+      resetFormFields()
       setFormSubmitted(true)
-    } else {
-      setRecaptchaFailed(true)
-      recaptchaRef.current?.reset()
+    } catch (error) {
+      setSendingForm(false)
+      setFormSubmitted(false)
+      if (
+        error instanceof Error &&
+        error.message === "ReCAPTCHA precisa ser resolvido"
+      ) {
+        alert(error.message)
+      }
+      alert(
+        "Erro ao enviar formulário. Tente recarregar a página e tentar novamente."
+      )
     }
   }
 
@@ -60,23 +63,30 @@ export function FormContact(): JSX.Element {
     return (
       <div className="w-full py-1 md:py-4 px-2 md:px-32 flex flex-col items-center justify-center text-center">
         <p className="text-lg text-black dark:text-white py-2 my-2">
-          Sua mensagem foi enviada com sucesso! Em breve entrarei em contato.
+          Sua mensagem foi enviada com sucesso!
+          <br />
+          Em breve entrarei em contato.
         </p>
       </div>
     )
   }
 
   return (
-    <div className="w-full py-1 md:py-4 px-2 md:px-32 flex flex-col items-center justify-center">
-      <h4 className="text-2xl font-bold text-black dark:text-white py-1 my-1">
-        Fale comigo
-      </h4>
+    <div className="w-full py-1 md:py-4 px-2 md:px-16 flex flex-col items-center justify-center">
+      <div className="w-full py-1 my-1">
+        <h4 className="text-2xl font-bold text-black dark:text-white">
+          Será um prazer conversar com você!
+        </h4>
+        <p className="text-gray-600 dark:text-gray-400">
+          Preencha o formulário abaixo e entrarei em contato o mais breve.
+        </p>
+      </div>
       <form
         onSubmit={handleSubmit(handleForm)}
-        className="items-start w-full justify-center flex flex-col space-y-4"
+        className="items-start w-full justify-center flex flex-col space-y-4 mt-4"
       >
         <div className="flex flex-col space-y-2 w-full">
-          <label htmlFor="name">Seu nome:</label>
+          <label htmlFor="name">Nome:</label>
           <input
             type="text"
             id="name"
@@ -87,8 +97,8 @@ export function FormContact(): JSX.Element {
             <span className="text-red-500">Nome é obrigatório</span>
           )}
         </div>
-        <div className="flex flex-col space-y-2 w-full ">
-          <label htmlFor="email">Seu e-mail:</label>
+        <div className="flex flex-col space-y-2 w-full">
+          <label htmlFor="email">E-mail:</label>
           <input
             type="email"
             id="email"
@@ -100,7 +110,7 @@ export function FormContact(): JSX.Element {
           )}
         </div>
         <div className="flex flex-col space-y-2 w-full">
-          <label htmlFor="message">Sua mensagem:</label>
+          <label htmlFor="message">Mensagem:</label>
           <textarea
             id="message"
             rows={4}
@@ -113,20 +123,17 @@ export function FormContact(): JSX.Element {
           )}
         </div>
         <div className="flex flex-col space-y-2 w-full">
-          <ReCAPTCHA
-            sitekey={siteKey}
-            ref={recaptchaRef}
-            onChange={() => setRecaptchaFailed(false)}
-            style={{
-              margin: "12px 0",
-              boxShadow: recaptchaFailed ? "0 0 20px -5px red" : "none",
-              width: "fit-content"
-            }}
-          />
+          {isDirty && (
+            <ReCAPTCHA
+              sitekey={siteKey}
+              ref={recaptchaRef}
+              onChange={() => setRecaptchaSubmitted(true)}
+            />
+          )}
         </div>
         <button
           type="submit"
-          disabled={!isDirty || !isValid || sendingForm}
+          disabled={!isDirty || !isValid || !recaptchaSubmitted}
           className="bg-black text-white dark:bg-white dark:text-black rounded-sm p-2 cursor-pointer transition duration-300 ease-in-out disabled:opacity-50 disabled:cursor-not-allowed"
         >
           <RiSendPlaneFill className="inline-block mr-2" />
