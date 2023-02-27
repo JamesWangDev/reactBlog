8a91d38e3bb0382e367d4b4c2d242d7cbc73002d
jameswang <james.wang.dev712@gmail.com>
2023-02-27 17:34:59 -0300

    perf: component content and style changed

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -1,36 +1,34 @@
+import { client } from "@lib/sanity.client"
+import urlBuilder from "@sanity/image-url"
 import { Project } from "@types"
 import Image from "next/image"
+import Link from "next/link"
 
 export function ProjectCard(props: Project): JSX.Element {
+  const builder = urlBuilder(client)
   return (
-    <div className="bg-cod-gray-100 max-w-xs border border-cod-gray-200 rounded-sm overflow-hidden shadow-md dark:bg-cod-gray-900 dark:border-cod-gray-700">
-      {props.image && (
-        <Image
-          src={props.image}
-          alt={props.name}
-          width={320}
-          height={120}
-          className="rounded-t-sm object-cover"
-        />
-      )}
-      <div className="m-2 p-2">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          {props.name}
-        </h5>
+    <div className="flex flex-col justify-center items-start">
+      <div className="my-2 py-2">
+        <Link href={`/projetos/${props.slug}`} target="_self">
+          <h5 className="min-w-max md:mr-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white hover:underline decoration-2 transition duration-300 ease-in-out">
+            {props.name}
+          </h5>
+        </Link>
         <p className="mb-2 text-gray-700 dark:text-gray-400">
           {props.description}
         </p>
-        <div className="flex flex-row pt-4 pb-2">
-          {props.technologies.map((technology) => (
-            <span
-              key={technology}
-              className="inline-block bg-gray-400 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 dark:bg-cod-gray-700 dark:text-gray-400"
-            >
-              #{technology.toLowerCase().replace(" ", "-")}
-            </span>
-          ))}
-        </div>
       </div>
+      <Link href={`/projetos/${props.slug}`} target="_self">
+        {props.image && (
+          <Image
+            src={builder.image(props.image).url()}
+            alt={props.name}
+            width={504}
+            height={282}
+            className="rounded-2xl object-cover w-[504px] h-[282px]"
+          />
+        )}
+      </Link>
     </div>
   )
 }
