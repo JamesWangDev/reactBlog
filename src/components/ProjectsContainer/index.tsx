743620940525da8e8f382fe22c9c6902a4b9545e
jameswang <james.wang.dev712@gmail.com>
2023-02-27 17:52:23 -0300

    perf: grid and responsiveness changes

--- src/components/ProjectsContainer/index.tsx
+++ src/components/ProjectsContainer/index.tsx
@@ -3,7 +3,7 @@ import { Project, ProjectsContainerProps } from "@types"
 
 export function ProjectsContainer(props: ProjectsContainerProps): JSX.Element {
   return (
-    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+    <div className="w-full min-h-max grid grid-cols-1 lg:grid-cols-2 gap-8 my-2 py-4">
       {props.projects.map((project: Project) => (
         <ProjectCard key={project.name} {...project} />
       ))}
