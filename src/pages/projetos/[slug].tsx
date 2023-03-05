a4e23a1de13ae223b094fb3f8f52673176e8fc59
jameswang <james.wang.dev712@gmail.com>
2023-03-05 18:28:09 -0300

    feat: `projetos` page created

---
+++ src/pages/projetos/[slug].tsx
@@ -0,0 +1,54 @@
+import { Footer } from "@components/Footer"
+import { HeadDefault } from "@components/HeadDefault"
+import { Header } from "@components/Header"
+import { Loading } from "@components/Loading"
+import { ProjectContent } from "@components/ProjectContent"
+import { SectionContainer } from "@components/SectionContainer"
+import { client } from "@lib/sanity.client"
+import { Project, ProjectContentProps } from "@types"
+import { sanityQueries } from "@utils/sanityQueries"
+import { useRouter } from "next/router"
+
+export async function getStaticProps(context: any) {
+  const { slug } = context.params
+  const [projectBySlug] = await client.fetch(sanityQueries.projectBySlug, {
+    slug
+  })
+  return {
+    props: {
+      project: projectBySlug
+    }
+  }
+}
+
+export async function getStaticPaths() {
+  const slugs = await client.fetch(sanityQueries.projectSlug)
+
+  const paths = slugs.map((project: Project) => ({
+    params: { slug: project.slug }
+  }))
+
+  return {
+    paths,
+    fallback: true
+  }
+}
+
+export default function Projeto(props: ProjectContentProps): JSX.Element {
+  const router = useRouter()
+
+  if (router.isFallback) {
+    return <Loading />
+  }
+
+  return (
+    <div className="bg-cod-gray-100 dark:bg-cod-gray-900 flex min-h-screen w-full flex-col items-center justify-between py-2 px-4 md:py-4 md:px-16">
+      <HeadDefault />
+      <Header />
+      <SectionContainer title={props.project.name}>
+        <ProjectContent project={props.project} />
+      </SectionContainer>
+      <Footer />
+    </div>
+  )
+}
---
+++ src/pages/projetos/index.tsx
@@ -0,0 +1,39 @@
+import { Footer } from "@components/Footer"
+import { HeadDefault } from "@components/HeadDefault"
+import { Header } from "@components/Header"
+import { ProjectsContent } from "@components/ProjectsContent"
+import { SectionContainer } from "@components/SectionContainer"
+import { client } from "@lib/sanity.client"
+import { sanityQueries } from "@utils/sanityQueries"
+import { ProjectProps, ProjectsGSP } from "@types"
+import * as React from "react"
+import { GlobalContext } from "@contexts/GlobalContext"
+
+export async function getStaticProps(): Promise<ProjectsGSP> {
+  const projects = await client.fetch(sanityQueries.projects)
+
+  return {
+    props: {
+      projects
+    }
+  }
+}
+
+export default function Projetos(props: ProjectProps): JSX.Element {
+  const { setProjects } = React.useContext(GlobalContext)
+
+  React.useEffect(() => {
+    setProjects(props.projects)
+  })
+
+  return (
+    <div className="bg-cod-gray-100 dark:bg-cod-gray-900 flex min-h-screen w-full flex-col items-center justify-between py-2 px-4 md:py-4 md:px-16">
+      <HeadDefault />
+      <Header />
+      <SectionContainer title="Projetos">
+        <ProjectsContent />
+      </SectionContainer>
+      <Footer />
+    </div>
+  )
+}
