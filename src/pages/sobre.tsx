ebb397337d342fc6c7b02c838df1eff1e367081d
jameswang <james.wang.dev712@gmail.com>
2023-03-12 13:58:31 -0300

    perf: SEO configured and `HeadDefault` component removed

--- src/pages/sobre.tsx
+++ src/pages/sobre.tsx
@@ -1,16 +1,25 @@
 import { AboutContent } from "@components/AboutContent"
 import { Footer } from "@components/Footer"
-import { HeadDefault } from "@components/HeadDefault"
 import { Header } from "@components/Header"
 import { SectionContainer } from "@components/SectionContainer"
+import { client } from "@lib/sanity.client"
+import { SobreGetStaticProps, SobreProps } from "@types"
+import { sanityQueries } from "@utils/sanityQueries"
 
-export default function Sobre(): JSX.Element {
+export async function getStaticProps(): Promise<SobreGetStaticProps> {
+  const [about] = await client.fetch(sanityQueries.about)
+
+  return {
+    props: { about }
+  }
+}
+
+export default function Sobre(props: SobreProps): JSX.Element {
   return (
     <div className="bg-cod-gray-100 dark:bg-cod-gray-900 flex min-h-screen w-full flex-col items-center justify-between py-2 px-4 md:py-4 md:px-16">
-      <HeadDefault />
       <Header />
       <SectionContainer title="Sobre Mim">
-        <AboutContent />
+        <AboutContent {...props.about} />
       </SectionContainer>
       <Footer />
     </div>
