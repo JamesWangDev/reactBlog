f4c7b80ddc91bc401392828cb8e1ac4192756c92
jameswang <james.wang.dev712@gmail.com>
2023-03-12 13:57:41 -0300

    perf: SEO configured and `HeadDefault` component removed

--- src/pages/projetos/index.tsx
+++ src/pages/projetos/index.tsx
@@ -1,5 +1,4 @@
 import { Footer } from "@components/Footer"
-import { HeadDefault } from "@components/HeadDefault"
 import { Header } from "@components/Header"
 import { ProjectsContent } from "@components/ProjectsContent"
 import { SectionContainer } from "@components/SectionContainer"
@@ -28,7 +27,6 @@ export default function Projetos(props: ProjectProps): JSX.Element {
 
   return (
     <div className="bg-cod-gray-100 dark:bg-cod-gray-900 flex min-h-screen w-full flex-col items-center justify-between py-2 px-4 md:py-4 md:px-16">
-      <HeadDefault />
       <Header />
       <SectionContainer title="Projetos">
         <ProjectsContent />
