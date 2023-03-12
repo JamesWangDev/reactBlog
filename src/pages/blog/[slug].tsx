c69837d474489b807d98a5595b8909200963bb97
jameswang <james.wang.dev712@gmail.com>
2023-03-12 13:56:44 -0300

    perf: SEO configured and `HeadDefault` component removed

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,5 +1,4 @@
 import { Footer } from "@components/Footer"
-import { HeadDefault } from "@components/HeadDefault"
 import { Header } from "@components/Header"
 import { Loading } from "@components/Loading"
 import { PostContainer } from "@components/PostContainer"
@@ -45,7 +44,6 @@ export default function Post(props: PostProps): JSX.Element {
 
   return (
     <div className="bg-cod-gray-100 dark:bg-cod-gray-900 flex min-h-screen w-full flex-col items-center justify-between py-2 px-4 md:py-4 md:px-16">
-      <HeadDefault />
       <Header />
       <PostContainer post={props.post} />
       <Footer />
