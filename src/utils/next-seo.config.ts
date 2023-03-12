bb144a07489ed436d98ca56692bef1aa0f6c312f
jameswang <james.wang.dev712@gmail.com>
2023-03-12 13:52:52 -0300

    feat: object responsible for meta tags created

---
+++ src/utils/next-seo.config.ts
@@ -0,0 +1,23 @@
+import { DefaultSeoProps } from 'next-seo';
+
+export const SEO: DefaultSeoProps = {
+  title: "Paulo Ruan - Desenvolvedor Web",
+  description: "Site pessoal de Paulo Ruan",
+  canonical: "https://pauloruan.vercel.app",
+  openGraph: {
+    type: "website",
+    locale: "pt_BR",
+    url: "https://pauloruan.vercel.app",
+    site_name: "Paulo Ruan",
+    title: "Paulo Ruan - Desenvolvedor Web",
+    description: "Site pessoal de Paulo Ruan",
+    images: [
+      {
+        url: "https://pauloruan.vercel.app/images/og-image.png",
+        width: 1280,
+        height: 720,
+        alt: "Paulo Ruan - Site pessoal de Paulo Ruan"
+      }
+    ]
+  }
+};
