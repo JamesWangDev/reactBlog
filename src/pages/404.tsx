a69d630c2f79dafbd9707b4029c165fed1b2e880
jameswang <james.wang.dev712@gmail.com>
2023-03-12 13:05:30 -0300

    perf: subtitle removed

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,4 @@
 import { Footer } from "@components/Footer"
-import { HeadDefault } from "@components/HeadDefault"
 import { Header } from "@components/Header"
 import { NotFoundContent } from "@components/NotFoundContent"
 import { SectionContainer } from "@components/SectionContainer"
@@ -7,12 +6,8 @@ import { SectionContainer } from "@components/SectionContainer"
 export default function NotFound(): JSX.Element {
   return (
     <div className="bg-cod-gray-100 dark:bg-cod-gray-900 flex min-h-screen w-full flex-col items-center justify-between py-2 px-4 md:py-4 md:px-16">
-      <HeadDefault />
       <Header />
-      <SectionContainer
-        title="Página não encontrada"
-        subtitle="Desculpa, não foi possível encontrar a página acessada."
-      >
+      <SectionContainer title="Página não encontrada">
         <NotFoundContent />
       </SectionContainer>
       <Footer />
