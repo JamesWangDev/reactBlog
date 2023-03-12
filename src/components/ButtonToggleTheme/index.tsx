a9d08d15f266b06061ce092449a6b5042902d77b
jameswang <james.wang.dev712@gmail.com>
2023-03-12 17:34:11 -0300

    refactor: `ButtonToggleTheme` component changed

--- src/components/ButtonToggleTheme/index.tsx
+++ src/components/ButtonToggleTheme/index.tsx
@@ -1,9 +1,13 @@
+import { Switch } from "@headlessui/react"
 import { useTheme } from "next-themes"
-import { MdDarkMode, MdLightMode } from "react-icons/md"
 
 export function ButtonToggleTheme(): JSX.Element {
   const { theme, setTheme, systemTheme } = useTheme()
   const currentTheme = theme === "system" ? systemTheme : theme
+  const spanIsDark = "md:translate-x-6 translate-x-4 bg-black"
+  const spanIsWhite = "translate-x-1 bg-white"
+  const styleSpan = currentTheme === "dark" ? spanIsDark : spanIsWhite
+
   function handleTheme(): void {
     if (currentTheme === "dark") {
       setTheme("light")
@@ -13,22 +17,19 @@ export function ButtonToggleTheme(): JSX.Element {
   }
 
   return (
-    <button
-      type="button"
-      onClick={() => handleTheme()}
-      className="h-8 w-8 no-underline flex items-center justify-center cursor-pointer p-1 mx-1"
-    >
-      {currentTheme === "dark" ? (
-        <MdLightMode
-          className="text-gray-600 font-semibold hover:text-white transition duration-300 ease-in-out"
-          size={48}
-        />
-      ) : (
-        <MdDarkMode
-          className="text-gray-400 font-semibold hover:text-black transition duration-300 ease-in-out"
-          size={48}
-        />
-      )}
-    </button>
+    <Switch.Group>
+      <div className="flex items-center">
+        <Switch.Label className="sr-only">Tema</Switch.Label>
+        <Switch
+          checked={currentTheme === theme}
+          onChange={handleTheme}
+          className="relative inline-flex h-4 w-8 md:h-6 md:w-11 items-center rounded-full transition-colors ease-in-out duration-200 bg-gray-600 dark:bg-gray-400"
+        >
+          <span
+            className={`inline-block h-3 w-3 md:h-4 md:w-4 transform rounded-full transition ${styleSpan}`}
+          />
+        </Switch>
+      </div>
+    </Switch.Group>
   )
 }
