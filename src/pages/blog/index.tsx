f3b06d3db52db84b4c7819dfdd6173f1aab6efdc
jameswang <james.wang.dev712@gmail.com>
2023-03-12 13:56:33 -0300

    perf: SEO configured and `HeadDefault` component removed

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,6 +1,5 @@
 import { BlogContent } from "@components/BlogContent"
 import { Footer } from "@components/Footer"
-import { HeadDefault } from "@components/HeadDefault"
 import { Header } from "@components/Header"
 import { SectionContainer } from "@components/SectionContainer"
 import { GlobalContext } from "@contexts/GlobalContext"
@@ -34,7 +33,6 @@ export default function Blog(props: BlogProps): JSX.Element {
 
   return (
     <div className="bg-cod-gray-100 dark:bg-cod-gray-900 flex min-h-screen w-full flex-col items-center justify-between py-2 px-4 md:py-4 md:px-16">
-      <HeadDefault />
       <Header />
       <SectionContainer title="Blog">
         <BlogContent />
